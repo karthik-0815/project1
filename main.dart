2ec619d90a46ea75513664a6863a71f68d5f2bb8
karthik-0815 <75531612+karthik-0815@users.noreply.github.com>
2021-11-20 13:36:43 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(
     Material(color: Colors.blue.shade700,
     child:Center(
-      child: Text("Welcome To Hire Us",
+      child: Text("Welcome Karthik",
       textDirection: TextDirection.ltr,
       style: TextStyle(color: Colors.white, fontSize: 40.0),
     )
